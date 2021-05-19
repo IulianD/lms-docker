052330b0fde3befb9f614700582ff674839d0437
IulianD <iulian_dragan@yahoo.com>
2021-05-19 08:27:21 +0200

    bug

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "First run setup..."
-/lms/first_run.sh
-mv /lms/first_run.sh /lms/first_run.sh.done
+/lms/first-run.sh
+mv /lms/first-run.sh /lms/first-run.sh.done
 getent group audio
 groupmod -g $AUDIOGROUP audio
-/usr/bin/start-container.sh
+/usr/bin/start-container
